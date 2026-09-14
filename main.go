d17705355d424828ccec1543a501595c9873ea29
mgzeg <m.gzegozevskis@eif.viko.lt>
2026-09-14 11:37:52 +0300

    week 2 syntax start

--- main.go
+++ main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+var age uint = 20
+
+func printNameAndAge(name string, age uint) {
+	fmt.Println("Hello", name)
+	fmt.Println("You are", age, "years old")
+}
+
 func main() {
-	fmt.Println("Hello world")
+
+	var name string
+	var age uint
+
+	fmt.Println("Enter your name:")
+	fmt.Scan(&name)
+
+	fmt.Println("Enter your age:")
+	fmt.Scan(&age)
+
+	printNameAndAge(name, age)
+
 }

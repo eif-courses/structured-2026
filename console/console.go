e0b8f8f90de02aceab42dd4b827f34bfdaa3ebb7
Marius <m.gzegozevskis@eif.viko.lt>
2026-09-16 11:21:37 +0300

    week 2 math lib added

--- console/console.go
+++ console/console.go
@@ -2,6 +2,7 @@ package console
 
 import (
 	"fmt"
+	"structured-2026/math"
 )
 
 type Person struct {
@@ -12,6 +13,10 @@ type Person struct {
 func (p *Person) PrintNameAndAge() {
 	fmt.Println("Hello", p.Name)
 	fmt.Println("You are", p.Age, "years old")
+
+	sum := math.AddTwoNumbers(10, 20)
+
+	fmt.Println("My total sum of 2 numbers:", sum)
 }
 
 func InitMyConsoleApp() {
